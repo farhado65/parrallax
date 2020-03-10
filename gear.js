e2b70e9a5fa12ca430e2d0701c1279e0fceeeae7
JavaScript <javascript@Admins-iMac-8.local>
2020-03-10 21:01:14 +0200

    Initial commit

---
+++ gear.js
@@ -0,0 +1,12 @@
+document
+    .querySelector('.container')
+    .addEventListener('scroll', rotate);
+
+    function rotate() {
+        let distance = document
+            .querySelector('.container')
+            .scrollTop;
+        document.querySelector('.gear')
+                .style.transform = `rotate(${distance}deg)`;
+
+    }
